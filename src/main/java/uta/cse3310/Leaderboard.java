6d51867ca5bb676c813c28527cac093d3b1392de
Zubair Rashaad <zubairashaad04@gmail.com>
2024-03-24 23:15:50 +0000

    ethodsInLeaderboard

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,5 +1,25 @@
 package uta.cse3310;
 
 public class Leaderboard{
+    public void displayLeaderboard(){
+        
+    }
+
+    public void updateScore(){
+
+    }
+    
+    public void resetLeaderboard(){
+
+    }
+
+    public int getRank(){
+
+    }
+
+    public sortPlayerByScore(){
+        
+    }
+
 
 }
