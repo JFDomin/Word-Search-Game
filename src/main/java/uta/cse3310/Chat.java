28c2a6376e08a062b7c539d31a69e98512162572
JFDomin <150066117+JFDomin@users.noreply.github.com>
2024-03-24 23:07:26 +0000

    added methods and variables

--- src/main/java/uta/cse3310/Chat.java
+++ src/main/java/uta/cse3310/Chat.java
@@ -1,5 +1,17 @@
 package uta.cse3310;
 
 public class Chat{
-    
+    public string Messages;
+
+    public void sendMessage(){
+
+    }
+
+    public List<String> receiveMessage(){
+
+    }
+
+    public void displayMessage(){
+        
+    }
 }
