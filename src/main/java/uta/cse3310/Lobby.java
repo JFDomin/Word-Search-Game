13941132a7cf9ac7ee979cab9af5f5b124287b91
prasannatiwari <157318975+prasannatiwari@users.noreply.github.com>
2024-03-24 18:30:53 -0500

    Update Lobby.java

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -1,5 +1,49 @@
 package uta.cse3310;
 
-public class Lobby{
+public class Lobby {
+    private int numPlayers;
+    private int playersWaiting;
+    private boolean ready;
 
+    public Lobby() {
+        numPlayers = 0;
+        playersWaiting = 0;
+        ready = false;
+    }
+
+    public void addPlayer() {
+        numPlayers++;
+        if (!ready) {
+            playersWaiting++;
+        }
+    }
+
+    public void checkLobby() {
+        System.out.println("Number of Players: " + numPlayers);
+        System.out.println("Players Waiting: " + playersWaiting);
+        System.out.println("Lobby Ready: " + ready);
+    }
+
+    public void generateLobby() {
+       
+    }
+
+    public void removePlayer() {
+        numPlayers--;
+        if (!ready) {
+            playersWaiting--;
+        }
+    }
+
+    public void joinGame() {
+       
+    }
+
+    public void exitGame() {
+       
+    }
+
+    public void setPlayerReady() {
+        ready = true;
+    }
 }
