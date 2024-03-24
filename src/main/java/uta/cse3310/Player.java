2660341e8afdfbf1e1545018f79a4d3c2d851bd1
JFDomin <150066117+JFDomin@users.noreply.github.com>
2024-03-24 23:41:54 +0000

    vi's update

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,5 +1,40 @@
 package uta.cse3310;
 
 public class Player{
+    public int playerID;
+    public string nickname;
+    public int playerScore;
+    public string playerColor;
+ 
+    public string getName() {
+ 
+    }
     
+    public boolean joinGame() {
+ 
+    }
+ 
+    public boolean leaveGame() {
+ 
+    }
+     
+    public void chooseStartLetter() {
+ 
+    }
+ 
+    public void chooseEndLetter() {
+ 
+    }
+ 
+    public void sendChat() {
+ 
+    }
+ 
+    public string assignColor() {
+ 
+    }
+ 
+    public int getPlayerScore() {
+    
+    }
 }
