327611293949d9428b7fcca9554a36763da83023
EEdosa <159320291+EEdosa@users.noreply.github.com>
2024-03-25 00:04:56 +0000

    updated WordBank.java

--- src/main/java/uta/cse3310/WordBank.java
+++ src/main/java/uta/cse3310/WordBank.java
@@ -1,5 +1,17 @@
 package uta.cse3310;
 
 public class WordBank extends WordGrid{
+    private int startPosition;
+    private int endpPosition;
+    private boolean found;
 
+    public class readFileIntoArray(){
+        return words;
+    }
+    public markFound(){
+        return true; 
+    }
+    public void displayWordBank (){
+
+    }
 }
