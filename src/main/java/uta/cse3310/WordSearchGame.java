83a03d7b0e6528058a751ee59ce49e63e30b488e
prasannatiwari <157318975+prasannatiwari@users.noreply.github.com>
2024-03-24 20:42:48 -0500

    Update WordSearchGame.java

--- src/main/java/uta/cse3310/WordSearchGame.java
+++ src/main/java/uta/cse3310/WordSearchGame.java
@@ -1,11 +1,24 @@
 package uta.cse3310;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class WordSearchGame{
     public int gameID;
     public ArrayList<Player> players;
     public double gridTimer;
     public double gameTimer;
+   public GameState gameState;
+   public Map<Player, Integer> scores;
+   public int maxPlayers;
+   public List<String> currentWords;
+   
+    public enum GameState {
+        NOT_STARTED,
+        IN_PROGRESS,
+        ENDED 
+    }
+    
     
     public boolean startGame() {
         return true;
@@ -22,4 +35,18 @@ public class WordSearchGame{
     public boolean selectWord() {
         return true;
     }
+    public int awardWord(){
+        return 0; 
+}
+    public boolean validateWord() {
+        return true;
+    }
+    public void updateGameState()
+    {
+    }
+
+public void displayPlayerStats()
+    {
+    }
+
 }
