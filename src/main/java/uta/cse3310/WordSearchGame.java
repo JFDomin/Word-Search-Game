da742fd029a0e732d7596a7eeaa6ec081acfd667
JFDomin <150066117+JFDomin@users.noreply.github.com>
2024-03-25 00:15:41 +0000

    added first half of methods and variables

--- src/main/java/uta/cse3310/WordSearchGame.java
+++ src/main/java/uta/cse3310/WordSearchGame.java
@@ -1,5 +1,24 @@
 package uta.cse3310;
 
 public class WordSearchGame{
+    public int gameID;
+    public ArrayList<Player> players;
+    public double gridTimer;
+    public double gameTimer;
     
+    public boolean startGame() {
+        return true;
+    }
+    
+    public boolean endGame() {
+        return true;
+    }
+ 
+    public void updateGameScores() {
+ 
+    }
+ 
+    public boolean selectWord() {
+        return true;
+    }
 }
