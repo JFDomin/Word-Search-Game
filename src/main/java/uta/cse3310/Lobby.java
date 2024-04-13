ec7a56ba1a9cec451d340ec25834dc4304cc35af
prasannatiwari <157318975+prasannatiwari@users.noreply.github.com>
2024-04-13 23:43:39 +0000

    Update Lobby

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -1,49 +1,81 @@
 package uta.cse3310;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lobby {
     private int numPlayers;
     private int playersWaiting;
     private boolean ready;
+    private List<Player> players;
 
     public Lobby() {
         numPlayers = 0;
         playersWaiting = 0;
         ready = false;
+        players = new ArrayList<>();
     }
 
-    public void addPlayer() {
+    public void addPlayer(Player player) {
+        players.add(player);
         numPlayers++;
-        if (!ready) {
-            playersWaiting++;
-        }
+        playersWaiting++;
+        checkLobby();
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+        numPlayers--;
+        playersWaiting--;
+        checkLobby();
+    }
+
+    public void joinGame(Player player) {
+        players.add(player);
+        numPlayers++;
+        playersWaiting++;
+        checkLobby();
+    }
+
+    public void exitGame(Player player) {
+        players.remove(player);
+        numPlayers--;
+        playersWaiting--;
+        checkLobby();
+    }
+
+    public void setPlayerReady(Player player) {
+        player.setReady(true);
+        checkLobby();
     }
 
     public void checkLobby() {
         System.out.println("Number of Players: " + numPlayers);
         System.out.println("Players Waiting: " + playersWaiting);
         System.out.println("Lobby Ready: " + ready);
-    }
-
-    public void generateLobby() {
-       
-    }
-
-    public void removePlayer() {
-        numPlayers--;
-        if (!ready) {
-            playersWaiting--;
+        if (numPlayers >= 2 && numPlayers <= 4 && allPlayersReady()) {
+            ready = true;
+        } else {
+            ready = false;
         }
     }
 
-    public void joinGame() {
-       
+    public void generateLobby() {
+        System.out.println("Players in the Lobby:");
+        for (Player player : players) {
+            System.out.println(player.getName());
+        }
+        System.out.println("Number of Players: " + numPlayers);
+        System.out.println("Players Waiting: " + playersWaiting);
+        System.out.println("Lobby Ready: " + ready);
     }
 
-    public void exitGame() {
-       
-    }
-
-    public void setPlayerReady() {
-        ready = true;
+    private boolean allPlayersReady() {
+        for (Player player : players) {
+            if (!player.isReady()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
