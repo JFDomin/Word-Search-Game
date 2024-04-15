8129f40d4ce475af63f72d7a591e77c912ac5034
jfd9842 <jfd9842@Ubuntu.myguest.virtualbox.org>
2024-04-15 02:12:50 -0500

    displayed board(disabled rn) and lobby (with static values)

--- html/uidisplay.js
+++ html/uidisplay.js
@@ -6,23 +6,18 @@
    // ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", ]
   //];
   function displayGrid(wordGrid) {
-    const wordGridcontainer = document.getElementById("wordGrid");
-    wordGridcontainer.innerHTML ="";
-    const gridSizerow = 50;
-    const gridSizecol = 50;
-    const grid = wordGrid;
-    const matrix = document.createElement("matrix");
-    for (let i = 0; i < gridSizerow; i++) {
-        const gridrow = document.createElement('gr');
-        for (let j = 0; j <gridSizecol; j++) {
-            const gridcol = document.createElement('gc');
-            gridcol.textContent = grid[i][j];
-            gridcol.style.border = "2px solid red"
-            
 
-            gridrow.appendChild(gridcol);
-        }
-        matrix.appendChild(gridrow);
-    }
-    wordGridcontainer.appendChild(table);
+      var html = '<table>';
+      wordGrid.forEach(function(row){
+        html += '<tr>';
+
+        row.forEach(function(cell){
+          html +='<td>' + cell + '</td>';
+        });
+
+        html += '</tr>';
+      });
+      html += '</table>';
+      console.log(html);
+      return html;
 }
