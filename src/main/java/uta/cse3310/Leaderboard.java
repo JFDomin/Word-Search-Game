273135d5ee4924ee66cf587e263daee646e3c919
prasannatiwari <157318975+prasannatiwari@users.noreply.github.com>
2024-04-15 23:58:22 -0500

    Update Leaderboard.java

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,46 +1,68 @@
 package uta.cse3310;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class leaderboardtest extends TestCase {
-    public leaderboardtest(String testName) {
-        super(testName);
+public class Leaderboard {
+    private List<Player> players;
+
+    public Leaderboard() {
+        this.players = new ArrayList<>();
     }
 
-    public static Test suite() {
-        return new TestSuite(leaderboardtest.class);
-    }
-
-    public void testScoreUpdatesAndRetrieval() {
-      
-        Leaderboard leaderboard = new Leaderboard();
-
-        // Test updating scores
-        leaderboard.updateScore(1, "PLayer1", 150, "Red");
-        leaderboard.updateScore(2, "PLayer2", 200, "Blue");
-
-        
-        assertEquals(150, leaderboard.getScore("Player1"));
-        assertEquals(200, leaderboard.getScore("PLayer2"));
-        assertEquals(0, leaderboard.getScore("PLayer"));
-
-        // Test getting all scores
-        Map<String, Integer> allScores = leaderboard.getAllScores();
-        assertEquals(2, allScores.size());
-        assertTrue(allScores.containsKey("PLayer1"));
-        assertTrue(allScores.containsKey("PLayer2"));
-        assertEquals(Integer.valueOf(150), allScores.get("Player1"));
-        assertEquals(Integer.valueOf(200), allScores.get("Player2"));
-
-     
-        System.out.println("----------------LEADERBOARD TESTING----------------");
-        System.out.println("Scores in the leaderboard: ");
-        for (Map.Entry<String, Integer> entry : allScores.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+    public void displayLeaderboard() { // SREQ007
+        System.out.println("Leaderboard:");
+        System.out.println("Rank\tName\tScore");
+        int rank = 1;
+        for (Player player : players) {
+            System.out.println(rank + "\t" + player.getName() + "\t" + player.getPlayerScore());
+            rank++;
         }
-        System.out.println("----------------TESTING COMPLETE-----------------");
+    }
+
+    public void updateScore(int playerID, String playerName, int newScore, String playerColor) { // SREQ014
+        for (Player player : players) {
+            if (player.getName().equals(playerName)) {
+                player.setPlayerScore(newScore);
+                sortPlayerByScore();
+                return;
+            }
+        }
+      
+        players.add(new Player(playerID, playerName, newScore, playerColor)); 
+        sortPlayerByScore();
+    }
+
+    public int getScore(String playerName) {
+        for (Player player : players) {
+            if (player.getName().equals(playerName)) {
+                return player.getPlayerScore();
+            }
+        }
+        return 0; // Player not found
+    }
+
+    public Map<String, Integer> getAllScores() {
+        Map<String, Integer> allScores = new HashMap<>();
+        for (Player player : players) {
+            allScores.put(player.getName(), player.getPlayerScore());
+        }
+        return allScores;
+    }
+
+    public int getRank(String playerName) { 
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getName().equals(playerName)) {
+                return i + 1; 
+            }
+        }
+        return -1; // Player not found
+    }
+
+    public void sortPlayerByScore() { 
+        Collections.sort(players, Comparator.comparingInt(Player::getPlayerScore).reversed());
     }
 }
